5b8ccf26db9800717f1a85543e282ce26173c839
Katamba-alfred <katambaalfred57@gmail.com>
2023-02-06 13:12:04 -0800

    Create post_form.php

---
+++ post_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="form2.css">
+	<title>Login</title>
+</head>
+<body>
+<form action = "handle_post.php" method = "post">
+<fieldset>
+    <legend>
+        <
+    </legend>
+</fieldset>
+</form>
+
+
+
+</body>
