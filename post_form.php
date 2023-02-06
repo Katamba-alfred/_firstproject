0333e04ba44aca7146f92b9a3d682610fe89e800
Katamba-alfred <katambaalfred57@gmail.com>
2023-02-06 13:38:26 -0800

    Update post_form.php

--- post_form.php
+++ post_form.php
@@ -10,8 +10,22 @@
 <form action = "handle_post.php" method = "post">
 <fieldset>
     <legend>
-        <
+        Post Form Handling
     </legend>
+    <p><label>Name: <input type="text" name="name" value="" size="20" maxlength="40"/></label></p>
+    <p><label>Email: <input type="text" name="email" value="" size="20" maxlength="40"/></label></p>
+    <p><label>Car Choice:
+        <input type="radio" name="car" value="Volvo"/>Volvo
+        <input type="radio" name="car" value="BMW"/>BMW
+        <input type="radio" name="car" value="Golf"/>Golf
+    </label></p>
+    <p><label>Companies:
+        <select name="companies">
+            <option value="VMC">Volvo Manufacturing Company</option>
+            <option value="BMW">Bavarian Engine Works</option>
+            <option value="Golf">Volkswagen company</option>
+        </select>
+    </label></p>
 </fieldset>
 </form>
 
